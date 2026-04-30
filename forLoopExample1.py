a6ea3cfdc9477579e3b0cea44edbfcfb3ee8b0d1
mshoaebansari <mshoaebansari@gmail.com>
2026-04-30 07:40:39 +0530

    A python file with for loop example is added

---
+++ forLoopExample1.py
@@ -0,0 +1,23 @@
+'''
+for loop
+loop statements are used to repeat a set statements
+
+example
+i want to display "welcome" on screen 10 times
+without loop
+print("welcome")
+print("welcome")
+....
+10 times
+
+for i in range(10): 0 1 2  3 4 5 6 7 8 9
+   print("welcome")
+
+range(start,end,step)
+range(0,10,1)
+
+'''
+for i in range(10):
+    print("welcome: ",i)
+
+
